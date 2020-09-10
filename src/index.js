f6913e627add232d70df9b0ddc7a74be150a952b
rafahmarinho <rafael.marinho96@hotmail.com>
2020-09-09 21:50:17 -0300

    atualizando index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use(routes);
 app.use(...swaggerConfig);
 
-app.get('/', (req, res) => res.json('Health check ok'));
+app.get('/', (req, res) => res.json('O pai ta on!'));
 
 app.listen(port, () => console.log('App running on port', port));
